db5acf3212e2e5e4e85b1c20be43365515943872
ksScott78 <Kieran.Scott@qa.com>
2017-11-30 16:59:20 +0000

    login and home page testing

--- src/test/java/HomePage.java
+++ src/test/java/HomePage.java
@@ -10,9 +10,35 @@ public class HomePage {
 
     @FindBy(css="#homepage-slider > div > div.bx-controls.bx-has-controls-direction > div > a.bx-next")
     WebElement nextCarossel;
-
     @FindBy(css="#homeslider > li:nth-child(2) > a")
-    WebElement imageReel;
+    WebElement image1;
+    @FindBy(css="#homeslider > li:nth-child(3) > a")
+    WebElement image2;
+    @FindBy(css="#homeslider > li:nth-child(4) > a")
+    WebElement image3;
+
+    @FindBy(css="#header > div:nth-child(3) > div > div > div:nth-child(3) > div > a > span.ajax_cart_quantity.unvisible")
+    WebElement cartLink;
+
+    @FindBy(css="#homefeatured > li.ajax_block_product.col-xs-12.col-sm-4.col-md-3.first-in-line.first-item-of-tablet-line.first-item-of-mobile-line > div > div.right-block > div.button-container > a.button.ajax_add_to_cart_button.btn.btn-default")
+    WebElement item1Add;
+
+    @FindBy(css = "#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > span > span")
+    WebElement continueShopping;
+    @FindBy(css="#homefeatured > li.ajax_block_product.col-xs-12.col-sm-4.col-md-3.first-in-line.first-item-of-tablet-line.first-item-of-mobile-line > div > div.left-block > div > a.product_img_link > img")
+    WebElement hoverItem1;
+
+    @FindBy(css="#header > div:nth-child(3) > div > div > div:nth-child(3) > div > a > span.ajax_cart_quantity")
+    WebElement cartSize;
+
+    public WebElement hoverItem1(){
+        return hoverItem1;
+    }
+
+
+    public String getCartSize(){
+        return cartSize.getAttribute("value");
+    }
 
     public void moveToLogin(){
         signIn.click();
@@ -22,7 +48,15 @@ public class HomePage {
         nextCarossel.click();
     }
 
-    public String getCurrentImg(){
-        return imageReel.getAttribute("title");
+    public String getImage1(){
+        return image1.getAttribute("title");
+    }
+
+    public String getImage2(){
+        return image2.getAttribute("title");
+    }
+
+    public String getImage3(){
+        return image3.getAttribute("title");
     }
 }
--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -2,17 +2,48 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import com.google.common.base.Function;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class HomePageTest {
 
     private String url = "http://automationpractice.com/index.php";
     private static WebDriver webDriver;
     private HomePage home;
-    private LoginPage login;
-    private SpreadSheetReader spreadSheetReader;
+    private Actions builder;
+
+
+
+
+   private void continueToShop(){
+       Wait<WebDriver> wait = new FluentWait<WebDriver>(webDriver).withTimeout(30,SECONDS).pollingEvery(5, SECONDS).ignoring(NoSuchElementException.class);
+
+       WebElement continueShopping = wait.until(new Function<WebDriver, WebElement>(){
+           public WebElement apply(WebDriver webDriver){
+               return webDriver.findElement(By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > span"));
+           }
+       });
+       continueShopping.click();
+   }
+
+
+    private void moveAndClick(WebElement target){
+        builder.moveToElement(target).click().release().perform();
+    }
+
 
     @Before
     public void setUp() {
@@ -20,6 +51,7 @@ public class HomePageTest {
         webDriver.manage().window().maximize();
         webDriver.navigate().to(url);
         home = PageFactory.initElements(webDriver,HomePage.class);
+        builder = new Actions(webDriver);
     }
 
     @After
@@ -31,17 +63,25 @@ public class HomePageTest {
         String img2 = "sample-2";
         String img3 = "sample-3";
 
-        Assert.assertEquals(img1, home.getCurrentImg());
+        Assert.assertEquals(img1, home.getImage1());
 
         home.nextImage();
-        Assert.assertEquals(img2, home.getCurrentImg());
+        Assert.assertEquals(img2, home.getImage2());
 
         home.nextImage();
-        Assert.assertEquals(img3, home.getCurrentImg());
+        Assert.assertEquals(img3, home.getImage3());
 
         home.nextImage();
-        Assert.assertEquals(img1, home.getCurrentImg());
+        Assert.assertEquals(img1, home.getImage1());
+    }
 
 
+
+    @Test
+    public void testAddToCart(){
+        builder.moveToElement(home.hoverItem1()).perform();
+        home.item1Add.click();
+        moveAndClick(home.continueShopping);
+        Assert.assertEquals("1", home.getCartSize());
     }
 }
